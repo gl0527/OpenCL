f05101f738e622d8bc00977b72917e24ee48641d
Lajos Gurzo <gurzolajos27@gmail.com>
2017-07-15 13:57:57 +0200

    Modify Makefile and coding style

--- gol/gol.cpp
+++ gol/gol.cpp
@@ -3,10 +3,10 @@
 #include "../Common.h"
 
 // kernel function
-const char* kernelString = STRINGIFY(
-    __kernel void gol(__global char* in, const int width, const int height, __global char* out)
+const char* kernelString = STRINGIFY (
+    __kernel void GOL (__global char* in, const int width, const int height, __global char* out)
     {
-        int2 threadID_2D = (int2)(get_global_id(0), get_global_id(1));
+        int2 threadID_2D = (int2) (get_global_id (0), get_global_id (1));
 
         if (threadID_2D.x < width && threadID_2D.y < height)
         {
@@ -26,7 +26,7 @@ const char* kernelString = STRINGIFY(
 );
 
 // global variables
-const char* kernelName      = "gol";
+const char* kernelName      = "GOL";
 
 const int WIDTH             = 800;
 const int HEIGHT            = 600;
@@ -36,17 +36,17 @@ bool keysPressed[256];
 const cl_uint workDim       = 2;
 size_t globalWorkSize[workDim];
 
-cl_platform_id platform     = NULL;
-cl_device_id device         = NULL;
-cl_context context          = NULL;
-cl_command_queue commands   = NULL;
-cl_program program          = NULL;
-cl_kernel kernel            = NULL;
+cl_platform_id platform     = nullptr;
+cl_device_id device         = nullptr;
+cl_context context          = nullptr;
+cl_command_queue commands   = nullptr;
+cl_program program          = nullptr;
+cl_kernel kernel            = nullptr;
 
-char* hostBuffer            = NULL;
-cl_float3* image            = NULL;
-cl_mem deviceBuffer         = NULL;
-cl_mem deviceBuffer_out     = NULL;
+char* hostBuffer            = nullptr;
+cl_float3* image            = nullptr;
+cl_mem deviceBuffer         = nullptr;
+cl_mem deviceBuffer_out     = nullptr;
 
 const cl_float3 aliveColor  = {1.0f, 1.0f, 0.0f};
 const cl_float3 deadColor   = {0.1f, 0.1f, 0.1f};
@@ -54,55 +54,55 @@ const cl_float3 deadColor   = {0.1f, 0.1f, 0.1f};
 cl_int err                  = CL_SUCCESS;
 
 // OpenCL
-bool initOpenCL(void)
+bool InitOpenCL (void)
 {
     // get available platforms - we want to get maximum 1 platform
-    err = clGetPlatformIDs(1, &platform, NULL);
-    if (!CheckCLError(err)) return false;
+    err = clGetPlatformIDs (1, &platform, nullptr);
+    if (!CheckCLError (err)) return false;
     
     const char MAXLENGTH = 40;
     char vendor[MAXLENGTH];
-    clGetPlatformInfo(platform, CL_PLATFORM_VENDOR, MAXLENGTH, vendor, NULL);
-    printf("Vendor: %s\n", vendor);
+    clGetPlatformInfo (platform, CL_PLATFORM_VENDOR, MAXLENGTH, vendor, nullptr);
+    printf ("Vendor: %s\n", vendor);
 
     // get available GPU devices - we want to get maximum 1 device
-    err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
-    if (!CheckCLError(err)) return false;
+    err = clGetDeviceIDs (platform, CL_DEVICE_TYPE_GPU, 1, &device, nullptr);
+    if (!CheckCLError (err)) return false;
 
     char deviceName[MAXLENGTH];
-    clGetDeviceInfo(device, CL_DEVICE_NAME, MAXLENGTH, &deviceName, NULL);
-    printf("Device name: %s\n", deviceName);
+    clGetDeviceInfo (device, CL_DEVICE_NAME, MAXLENGTH, &deviceName, nullptr);
+    printf ("Device name: %s\n", deviceName);
 
     // creation of OpenCL context with given properties
     cl_context_properties props[] = { 
         CL_CONTEXT_PLATFORM, (cl_context_properties)platform, 0 
     };
-    context = clCreateContext(props, 1, &device, NULL, NULL, &err);
-    if(!context || !CheckCLError(err)) return false;
+    context = clCreateContext (props, 1, &device, nullptr, nullptr, &err);
+    if (context == nullptr || !CheckCLError (err)) return false;
 
     // creation of OpenCL command queue with the CL_QUEUE_PROFILING_ENABLE property
-    commands = clCreateCommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &err);
-    if(!commands || !CheckCLError(err)) return false;
+    commands = clCreateCommandQueue (context, device, CL_QUEUE_PROFILING_ENABLE, &err);
+    if (commands == nullptr || !CheckCLError (err)) return false;
 
     // creation of the program
-    program = clCreateProgramWithSource(context, 1, &kernelString, NULL, &err);
-    if (!CheckCLError(err)) return false;
+    program = clCreateProgramWithSource (context, 1, &kernelString, nullptr, &err);
+    if (!CheckCLError (err)) return false;
 
     // compilation of the program
-    err = clBuildProgram(program, 1, &device, NULL, NULL, NULL);
-    if (!CheckCLError(err))
+    err = clBuildProgram (program, 1, &device, nullptr, nullptr, nullptr);
+    if (!CheckCLError (err))
     {
-        size_t logLength;
+        size_t logLength = 0;
         char* log = nullptr;
-        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &logLength);
+        clGetProgramBuildInfo (program, device, CL_PROGRAM_BUILD_LOG, 0, nullptr, &logLength);
         try {
             log = new char[logLength];
-        } catch (std::bad_alloc& ba) {
-            std::cerr << "Bad alloc exception was caught:" << ba.what() << '\n';
+        } catch (const std::bad_alloc& ba) {
+            std::cerr << "Bad alloc exception was caught:" << ba.what () << '\n';
 
             return false;
         }
-        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, logLength, log, 0);
+        clGetProgramBuildInfo (program, device, CL_PROGRAM_BUILD_LOG, logLength, log, 0);
         std::cout << log << std::endl;
         if (log != nullptr)
             delete[] log;
@@ -111,8 +111,8 @@ bool initOpenCL(void)
     }
 
     // creation of the kernel
-    kernel = clCreateKernel(program, kernelName, &err);
-    if(!CheckCLError(err)) return false;
+    kernel = clCreateKernel (program, kernelName, &err);
+    if (!CheckCLError (err)) return false;
 
     // initialization of host and device data
     globalWorkSize[0] = WIDTH;
@@ -121,64 +121,64 @@ bool initOpenCL(void)
     try {
         image = new cl_float3[NUM_OF_CELLS];
         hostBuffer = new char[NUM_OF_CELLS];
-    } catch (std::bad_alloc& ba) {
-        std::cerr << "Bad alloc exception was caught: " << ba.what() << '\n';
+    } catch (const std::bad_alloc& ba) {
+        std::cerr << "Bad alloc exception was caught: " << ba.what () << '\n';
 
         return false;
     }
 
     for (int i = 0; i < NUM_OF_CELLS; ++i)
-        hostBuffer[i] = ((float) rand() / RAND_MAX < 0.3) ? 1 : 0;
+        hostBuffer[i] = ( (float) rand () / RAND_MAX < 0.3) ? 1 : 0;
 
-    deviceBuffer = clCreateBuffer(context, CL_MEM_READ_WRITE, NUM_OF_CELLS, NULL, &err);
-    if (!deviceBuffer || !CheckCLError(err)) return false;
+    deviceBuffer = clCreateBuffer (context, CL_MEM_READ_WRITE, NUM_OF_CELLS, nullptr, &err);
+    if (deviceBuffer == nullptr || !CheckCLError (err)) return false;
 
-    deviceBuffer_out = clCreateBuffer(context, CL_MEM_READ_WRITE, NUM_OF_CELLS, NULL, &err);
-    if (!deviceBuffer_out || !CheckCLError(err)) return false;
+    deviceBuffer_out = clCreateBuffer (context, CL_MEM_READ_WRITE, NUM_OF_CELLS, nullptr, &err);
+    if (deviceBuffer_out == nullptr || !CheckCLError (err)) return false;
 
-    err = clEnqueueWriteBuffer(commands, deviceBuffer, CL_TRUE, 0, NUM_OF_CELLS, hostBuffer, 0, NULL, NULL);
-    if (!CheckCLError(err)) return false;
+    err = clEnqueueWriteBuffer (commands, deviceBuffer, CL_TRUE, 0, NUM_OF_CELLS, hostBuffer, 0, nullptr, nullptr);
+    if (!CheckCLError (err)) return false;
     
     return true;
 }
 
-void runOpenCL(void)
+void RunOpenCL (void)
 {
     // setting the kernel arguments
     err = 0;
-    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &deviceBuffer); 
-    err |= clSetKernelArg(kernel, 1, sizeof(int), &WIDTH); 
-    err |= clSetKernelArg(kernel, 2, sizeof(int), &HEIGHT); 
-    err |= clSetKernelArg(kernel, 3, sizeof(cl_mem), &deviceBuffer_out); 
-    if (!CheckCLError(err)) exit(-1);
+    err = clSetKernelArg (kernel, 0, sizeof (cl_mem), &deviceBuffer); 
+    err |= clSetKernelArg (kernel, 1, sizeof (int), &WIDTH); 
+    err |= clSetKernelArg (kernel, 2, sizeof (int), &HEIGHT); 
+    err |= clSetKernelArg (kernel, 3, sizeof (cl_mem), &deviceBuffer_out); 
+    if (!CheckCLError (err)) exit (-1);
 
     // kernel execution
-    err = clEnqueueNDRangeKernel(commands, kernel, workDim, NULL, globalWorkSize, NULL, 0, NULL, NULL);
-    if (!CheckCLError(err)) exit(-1);
+    err = clEnqueueNDRangeKernel (commands, kernel, workDim, nullptr, globalWorkSize, nullptr, 0, nullptr, nullptr);
+    if (!CheckCLError (err)) exit (-1);
 
     // getting back the results
-    clFinish(commands);
-    err = clEnqueueReadBuffer(commands, deviceBuffer_out, CL_TRUE, 0, NUM_OF_CELLS, hostBuffer, 0, NULL, NULL);
-    if (!CheckCLError(err)) exit(-1);
+    clFinish (commands);
+    err = clEnqueueReadBuffer (commands, deviceBuffer_out, CL_TRUE, 0, NUM_OF_CELLS, hostBuffer, 0, nullptr, nullptr);
+    if (!CheckCLError (err)) exit (-1);
 
     // swap the device buffers for the next step of the computation
-    std::swap(deviceBuffer, deviceBuffer_out);
+    std::swap (deviceBuffer, deviceBuffer_out);
     
     // updating and drawing the image
     for (int i = 0; i < NUM_OF_CELLS; ++i)
         image[i] = (hostBuffer[i] == 1) ? aliveColor : deadColor;
-    glDrawPixels(WIDTH, HEIGHT, GL_RGBA, GL_FLOAT, image);
+    glDrawPixels (WIDTH, HEIGHT, GL_RGBA, GL_FLOAT, image);
 }
 
-void destroyOpenCL(void)
+void DestroyOpenCL (void)
 {
     // free data
-    clReleaseKernel(kernel);
-    clReleaseProgram(program);
-    clReleaseMemObject(deviceBuffer_out);
-    clReleaseMemObject(deviceBuffer);
-    clReleaseCommandQueue(commands);
-    clReleaseContext(context);
+    clReleaseKernel (kernel);
+    clReleaseProgram (program);
+    clReleaseMemObject (deviceBuffer_out);
+    clReleaseMemObject (deviceBuffer);
+    clReleaseCommandQueue (commands);
+    clReleaseContext (context);
     if (hostBuffer != nullptr)
         delete[] hostBuffer;
     if (image != nullptr)
@@ -186,74 +186,83 @@ void destroyOpenCL(void)
 }
 
 // OpenGL
-void initOpenGL(void)
+void InitOpenGL (void)
 {
-    glClearColor(0.17f, 0.4f, 0.6f, 1.0f);
-    glDisable(GL_DEPTH_TEST);
+    glClearColor (0.17f, 0.4f, 0.6f, 1.0f);
+    glDisable (GL_DEPTH_TEST);
 }
 
-void display(void) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+void Display (void)
+{
+    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    runOpenCL();
+    RunOpenCL ();
 
-    glutSwapBuffers();
+    glutSwapBuffers ();
 }
 
-void idle(void) {
-    glutPostRedisplay();
+void Idle (void)
+{
+    glutPostRedisplay ();
 }
 
-void keyDown(unsigned char key, int x, int y) {
+void KeyDown (unsigned char key, int /*x*/, int /*y*/)
+{
     keysPressed[key] = true;
 }
 
-void keyUp(unsigned char key, int x, int y) {
+void KeyUp (unsigned char key, int /*x*/, int /*y*/)
+{
     keysPressed[key] = false;
-    switch (key) {
+    switch (key)
+    {
         case 27:
-            destroyOpenCL();
-            exit(0);
+            DestroyOpenCL ();
+            exit (0);
             break;
     }
 }
 
-void mouseClick(int button, int state, int x, int y) {
+void MouseClick (int /*button*/, int /*state*/, int /*x*/, int /*y*/)
+{
 
 }
 
-void mouseMove(int x, int y) {
+void MouseMove (int /*x*/, int /*y*/)
+{
 
 }
 
-void reshape(int newWidth, int newHeight) {
+void Reshape (int /*newWidth*/, int /*newHeight*/)
+{
 
 }
 
-int main(int argc, char* argv[]) {
-    srand(time(0));
+int main (int argc, char** argv)
+{
+    srand (time (0));
 
-    if (!initOpenCL())
+    if (InitOpenCL () == false)
         return 1;
 
-    glutInit(&argc, argv);
-    glutInitContextVersion(3, 0);
-    glutInitContextFlags(GLUT_CORE_PROFILE | GLUT_DEBUG);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
-    glutInitWindowSize(WIDTH, HEIGHT);
-    glutCreateWindow("Game of life");
+    glutInit                (&argc, argv);
+    glutInitContextVersion  (3, 0);
+    glutInitContextFlags    (GLUT_CORE_PROFILE | GLUT_DEBUG);
+    glutInitDisplayMode     (GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+    glutInitWindowSize      (WIDTH, HEIGHT);
+    glutCreateWindow        ("Game of life");
 
-    initOpenGL();
+    InitOpenGL ();
 
-    glutDisplayFunc(display);
-    glutIdleFunc(idle);
-    glutReshapeFunc(reshape);
-    glutKeyboardFunc(keyDown);
-    glutKeyboardUpFunc(keyUp);
-    glutMouseFunc(mouseClick);
-    glutMotionFunc(mouseMove);
+    glutDisplayFunc     (Display);
+    glutIdleFunc        (Idle);
+    glutReshapeFunc     (Reshape);
+    glutKeyboardFunc    (KeyDown);
+    glutKeyboardUpFunc  (KeyUp);
+    glutMouseFunc       (MouseClick);
+    glutMotionFunc      (MouseMove);
 
-    glutMainLoop();
+    glutMainLoop ();
 
     return 0;
 }
